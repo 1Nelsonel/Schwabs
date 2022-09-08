cebd2f70cfec2b8ab0a4ab31219814f7768b07fe
1Nelsonel <nelsonmokoro37@gmail.com>
2022-09-08 13:39:59 +0300

    success

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('about/', views.about, name='about'),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'base/contact.html', context)
+
+def appointment(request):
+    context = {}
+    return render(request, 'base/appointment.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'base/about.html', context)
