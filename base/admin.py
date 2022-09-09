4dc01bcfe50f994960d930821572bd62df90655b
1Nelsonel <nelsonmokoro37@gmail.com>
2022-09-09 11:10:18 +0300

    success

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Appointment, Blog, Comment, Contact, Service
 
 # Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Service)
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(Appointment)
